94d19c42340425e054f791fd97ccd7251825d5d8
Uzzal Devkota <theoozzal@gmail.com>
2014-08-16 11:41:50 +0545

    ng-cordova fix.

--- www/lib/ionic/js/ng-cordova.js
+++ www/lib/ionic/js/ng-cordova.js
@@ -1,7 +1,40 @@
 
-angular.module('ngCordova', [
-  'ngCordova.plugins'
+angular.module('ngCordova', ['ngCordova.plugins']);
+
+angular.module('ngCordova.plugins', [
+  'ngCordova.plugins.deviceMotion',
+  'ngCordova.plugins.camera',
+  'ngCordova.plugins.geolocation',
+  'ngCordova.plugins.deviceOrientation',
+  'ngCordova.plugins.dialogs',
+  'ngCordova.plugins.vibration',
+  'ngCordova.plugins.network',
+  'ngCordova.plugins.device',
+  'ngCordova.plugins.barcodeScanner',
+  'ngCordova.plugins.splashscreen',
+  'ngCordova.plugins.keyboard',
+  'ngCordova.plugins.contacts',
+  'ngCordova.plugins.statusbar',
+  'ngCordova.plugins.file',
+  'ngCordova.plugins.socialSharing',
+  'ngCordova.plugins.globalization',
+  'ngCordova.plugins.sqlite',
+  'ngCordova.plugins.ga',
+  'ngCordova.plugins.push',
+  'ngCordova.plugins.spinnerDialog',
+  'ngCordova.plugins.sms',
+  'ngCordova.plugins.pinDialog',
+  'ngCordova.plugins.localNotification',
+  'ngCordova.plugins.toast',
+  'ngCordova.plugins.flashlight',
+  'ngCordova.plugins.capture',
+  // 'ngCordova.plugins.appAvailability',
+  // 'ngCordova.plugins.prefs',
+  // 'ngCordova.plugins.printer',
+  'ngCordova.plugins.bluetoothSerial',
+  // 'ngCordova.plugins.backgroundGeolocation'
 ]);
+
 angular.module('ngCordova.plugins.sqlite', [])
 
   .factory('$cordovaSQLite', ['$q', function ($q) {
@@ -1419,39 +1452,7 @@ angular.module('ngCordova.plugins.capture', [])
     }
   }
 }]);
-angular.module('ngCordova.plugins', [
-  'ngCordova.plugins.deviceMotion',
-  'ngCordova.plugins.camera',
-  'ngCordova.plugins.geolocation',
-  'ngCordova.plugins.deviceOrientation',
-  'ngCordova.plugins.dialogs',
-  'ngCordova.plugins.vibration',
-  'ngCordova.plugins.network',
-  'ngCordova.plugins.device',
-  'ngCordova.plugins.barcodeScanner',
-  'ngCordova.plugins.splashscreen',
-  'ngCordova.plugins.keyboard',
-  'ngCordova.plugins.contacts',
-  'ngCordova.plugins.statusbar',
-  'ngCordova.plugins.file',
-  'ngCordova.plugins.socialSharing',
-  'ngCordova.plugins.globalization',
-  'ngCordova.plugins.sqlite',
-  'ngCordova.plugins.ga',
-  'ngCordova.plugins.push',
-  'ngCordova.plugins.spinnerDialog',
-  'ngCordova.plugins.sms',
-  'ngCordova.plugins.pinDialog',
-  'ngCordova.plugins.localNotification',
-  'ngCordova.plugins.toast',
-  'ngCordova.plugins.flashlight',
-  'ngCordova.plugins.capture',
-  'ngCordova.plugins.appAvailability',
-  'ngCordova.plugins.prefs',
-  'ngCordova.plugins.printer',
-  'ngCordova.plugins.bluetoothSerial',
-  'ngCordova.plugins.backgroundGeolocation'
-]);
+
 angular.module('ngCordova.plugins.device', [])
 
 .factory('$cordovaDevice', [function () {
