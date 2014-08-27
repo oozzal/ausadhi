0d1c060c1c923feb9290e3fe1009080641e086c8
oozzal <theoozzal@gmail.com>
2014-08-27 17:13:31 +0545

    Add sqlite plugin.

--- www/js/app.js
+++ www/js/app.js
@@ -4,13 +4,20 @@ angular.module('ausadhi', ['ionic', 'ngCordova', 'ausadhi.controllers', 'ausadhi
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
       // Set the statusbar to use the default style, tweak this to
       // remove the status bar on iOS or change it to use white instead of dark colors.
       StatusBar.styleDefault();
+    }
+
+    if(window.sqlitePlugin) {
+      var db = window.sqlitePlugin.openDatabase({name: 'ausadhi.db'});
+      db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS medicines (id INTEGER PRIMARY KEY, name VARCHAR, description TEXT, times INTEGER, start_at VARCHAR)');
+      });
     }
   });
 }])
